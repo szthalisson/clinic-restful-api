5e8afcf36b89b4cbaa47200fb48d2c3b80f0ea90
Thalisson Menezes <thalissons.menezes@gmail.com>
2025-01-31 20:03:32 -0300

    Create Patient Model

--- api/models.py
+++ api/models.py
@@ -14,3 +14,24 @@ class Doctor(models.Model):
 
   def __str__(self):
     return self.name
+  
+
+class Patient(models.Model):
+  code = models.IntegerField(null=True, blank=True, unique=True, verbose_name='Código')
+  name = models.CharField(max_length=100, verbose_name='Nome')
+  cpf = models.CharField(max_length=15, verbose_name='CPF')
+  address = models.TextField(verbose_name='Endereço')
+  phone = models.CharField(max_length=15, verbose_name='Telefone')
+  date_birth = models.DateField(verbose_name='Data de nascimento')
+
+  class Meta:
+    verbose_name = 'Paciente'
+    ordering = ['code']
+
+  def __str__(self):
+    return self.name
+
+  def save(self, *args, **kwargs):
+    if not self.code:
+      last_code = Patient.objects.order_by('-code').first()
+      self.code = (last_code + 1) if last_code else 100
