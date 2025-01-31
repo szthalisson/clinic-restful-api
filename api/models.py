d0479005b35f9661157b2fceb018d9b17c688880
Thalisson Menezes <thalissons.menezes@gmail.com>
2025-01-31 19:20:24 -0300

    Create doctor model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+  cpf = models.CharField(max_length=15, verbose_name='CPF', unique=True)
+  name = models.CharField(max_length=100, verbose_name='Nome')
+  admission = models.DateTimeField(verbose_name='Data de admissão')
+  wage = models.FloatField(verbose_name='Salário', default=4000)
+
+  class Meta:
+    verbose_name = 'Doutor'
+    verbose_name_plural = 'Doutores'
+    ordering = ['name']
+
+  def __str__(self):
+    return self.name
