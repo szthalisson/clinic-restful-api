a09b5cf8791224caa45b0155d4e0680fe914af2b
Thalisson Menezes <thalissons.menezes@gmail.com>
2025-01-31 20:06:13 -0300

    Admin Patient create

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from api.models import Doctor
+from api.models import Doctor, Patient
 
 
 class DoctorAdmin(admin.ModelAdmin):
   list_display = ['cpf', 'name', 'admission', 'wage']
   search_fields = ('name','cpf',)
 
+
+class PatientAdmin(admin.ModelAdmin):
+  list_display = ['code', 'name', 'phone']
+  search_fields = ('code','name','cpf',)
+
+
 admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Patient, PatientAdmin)
